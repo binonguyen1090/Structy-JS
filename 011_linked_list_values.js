eedc80118c7bb6b47b0f78a2361cb3ab0c1983ee
Bino <binonguyen1090@gmail.com>
2021-09-23 23:04:14 -0500

    011

---
+++ 011_linked_list_values.js
@@ -0,0 +1,66 @@
+// linked list values
+// Write a function, linkedListValues, that takes in the head of a linked list as an argument. The function should return an array containing all values of the nodes in the linked list.
+
+// test_00:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+
+// // a -> b -> c -> d
+
+// linkedListValues(a); // -> [ 'a', 'b', 'c', 'd' ]
+// test_01:
+// const x = new Node("x");
+// const y = new Node("y");
+
+// x.next = y;
+
+// // x -> y
+
+// linkedListValues(x); // -> [ 'x', 'y' ]
+// test_02:
+// const q = new Node("q");
+
+// // q
+
+// linkedListValues(q); // -> [ 'q' ]
+// test_03:
+// linkedListValues(null); // -> [ ]
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListValues = (head) => {
+    // todo
+    var result = []
+    
+    var current = head
+    while(current != null){
+      result.push(current.val)
+      current = current.next
+    }
+    return result
+  };
+  
+  
+  
+  const linkedListValues = (head, r = []) => {
+    // todo
+    
+    if (head != null){
+      r.push(head.val)
+      linkedListValues(head.next,r)
+    }
+    return r
+  };
+  
+  
