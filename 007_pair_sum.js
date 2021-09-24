e4c95cf4eff644aa62d068be4a476fa499bc0fbd
Bino <binonguyen1090@gmail.com>
2021-09-23 23:00:26 -0500

    007

---
+++ 007_pair_sum.js
@@ -0,0 +1,33 @@
+// pair sum
+// Write a function, pairSum, that takes in an array and a target sum as arguments. The function should return an array containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair that sums to the target.
+
+// test_00:
+// pairSum([3, 2, 5, 4, 1], 8); // -> [0, 2]
+// test_01:
+// pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
+// test_02:
+// pairSum([4, 7, 9, 2, 5, 1], 3); // -> [3, 5]
+// test_03:
+// pairSum([1, 6, 7, 2], 13); // -> [1, 2]
+// test_04:
+// pairSum([9, 9], 18); // -> [0, 1]
+// test_05:
+// pairSum([6, 4, 2, 8 ], 12); // -> [1, 3]
+
+const pairSum = (numbers, targetSum) => {
+    // todo
+    for (var i = 0; i < numbers.length - 1; i++){
+      for (var j = i + 1; j < numbers.length; j++){
+        var result = numbers[i] + numbers[j]
+        if(result == targetSum)
+          return [i,j]
+      }
+    }
+  };
+  
+  
+  
