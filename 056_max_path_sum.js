d77e7344196d4ff96ff40bcc6f839bcc0a009785
Bino <binonguyen1090@gmail.com>
2021-10-04 22:15:04 -0500

    056

---
+++ 056_max_path_sum.js
@@ -0,0 +1,100 @@
+// max path sum
+// Write a function, maxPathSum, that takes in a grid as an argument. The function should return the maximum sum possible by traveling a path from the top-left corner to the bottom-right corner. You may only travel through the grid by moving down or right.
+
+// You can assume that all numbers are non-negative.
+
+// test_00:
+// const grid = [
+//   [1, 3, 12],
+//   [5, 1, 1],
+//   [3, 6, 1],
+// ];
+// maxPathSum(grid); // -> 18
+// test_01:
+// const grid = [
+//   [1, 2, 8, 1],
+//   [3, 1, 12, 10],
+//   [4, 0, 6, 3],
+// ];
+// maxPathSum(grid); // -> 36
+// test_02:
+// const grid = [
+//   [1, 2, 8, 1],
+//   [3, 10, 12, 10],
+//   [4, 0, 6, 3],
+// ];
+// maxPathSum(grid); // -> 39
+// test_03:
+// const grid = [
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+// ];
+// maxPathSum(grid); // -> 27
+// test_04:
+// const grid = [
+//   [1, 1, 3, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 2, 1, 1, 6, 1, 1, 5, 1, 1, 0, 0, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 5, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+//   [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [2, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1],
+//   [2, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+// ];
+// maxPathSum(grid); // -> 56
+
+const maxPathSum = (grid, r=0,c= 0,hash={}) => {
+    // todo
+    let pos = r + ',' + c
+    if(pos in hash) return hash[pos]
+    if(r==grid.length || c==grid[r].length) return -Infinity
+    if(r== grid.length - 1 && c == grid[r].length -1 ) return grid[r][c]
+    
+    let right = maxPathSum(grid, r,c+1,hash)
+    let down = maxPathSum(grid, r+1,c,hash)
+    hash[pos] = Math.max(right, down) + grid[r][c]
+    return hash[pos]
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  module.exports = {
+    maxPathSum
+  };
